df419477e279b92ffd82c85af93ae2e16ef417a2
Mohanad Elhag <moh.7afiz@gmail.com>
2017-09-07 01:14:08 +0300

    Added components

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,30 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <router-view></router-view>
+    <h1>Word Translate</h1>
+    <h5>Powered By Vue.js</h5>
+    <TranslateForm v-on:formSubmit="translateText"></TranslateForm>
   </div>
 </template>
 
 <script>
+import TranslateForm from './components/TranslateForm'
+
 export default {
-  name: 'app'
+  name: 'app',
+  components: {
+    TranslateForm
+  },
+  methods: {
+    translateText: function(text) {
+      this.$http.get(https://translate.yandex.net/api/v1.5/tr.json/translate ?
+key='trnsl.1.1.20170906T180110Z.f13837ac92b42a4f.7fcbabbe589070799c4c4dd716388f1457a89374&lang=ar&text='+text);
+    }
+  }
 }
 </script>
 
 <style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+
 }
 </style>
---
+++ src/components/TranslateForm.vue
@@ -0,0 +1,31 @@
+<template>
+  <div id="translateFrom">
+    <form v-on:submit="formSubmit">
+      <input type="text" v-model="textToTranslate" placeholder="Enter a word....">
+      <input type="submit" name="" value="Translate">
+    </form>
+  </div>
+</template>
+
+<script>
+
+
+export default {
+  name: 'translateForm',
+  data() {
+    return {
+      textToTranslate: ''
+    }
+  },
+  methods: {
+    formSubmit(e){
+      this.$emit('formSubmit', this.textToTranslate);
+      e.preventDefault();
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
